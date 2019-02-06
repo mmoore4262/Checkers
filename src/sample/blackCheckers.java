91884d3c4aa482b91afad4bfff67d2f4e0b31755
chrisss101 <Crafaniello9535@bths.edu>
2019-02-06 09:00:43 -0500

    drthhhhr

---
+++ src/sample/blackCheckers.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class blackCheckers {
+    public int xpos;
+    public int ypos;
+    public blackCheckers(int xpos, int ypos,String color) {
+        this.xpos= xpos;
+        this.ypos = ypos;
+    }
+}
---
+++ src/sample/redCheckers.java
@@ -0,0 +1,46 @@
+package sample;
+
+public class redCheckers {
+    public int xpos;
+    public int ypos;
+    public redCheckers(int xpos, int ypos,String color) {
+        this.xpos= xpos;
+        this.ypos = ypos;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
