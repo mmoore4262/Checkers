204135f9d5e33e8c10bce82fe625fe8ef25882d3
BT_1N3_34 <pyu6253>
2019-02-11 10:10:18 -0500

    pro

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         String color = "";
+        String img = "";
         double sceneWidth = 300;
         double sceneHeight = 300;
         double gridWidth = sceneWidth / 8;
@@ -32,35 +33,70 @@ public class Main extends Application {
         for(int x=0; x < board.length; x++) {
             for(int y = 0; y < board[x].length; y++) {
                 color = "";
+                img = "";
                 if((x+y)%2 == 0 && y <= 3)
                 {
                      color = "black";
+                     img = "images/blackcheckers.png";
                 }
                 else if((x+y)%2 == 0 && y >= 6)
                 {
                     color = "red";
+                    img = "images/redcheckers.png";
                 }
                 CheckerPiece piece = new CheckerPiece(color, x, y);
-                Spaces node = new Spaces(x, y, piece, x * gridWidth, y * gridHeight, gridWidth, gridHeight);
+                Spaces node = new Spaces(x, y, piece, x * gridWidth, y * gridHeight, gridWidth, gridHeight, img);
                 root.getChildren().add(node);
-                board[x][y] = node;
+                if(color.equals("black") || color.equals("red")) {
+                    board[x][y] = node;
+                }
+                else
+                {
+                    board[x][y] = null;
+                }
             }
         }
+
         root.getChildren().add((Node) FXMLLoader.load(getClass().getResource("sample.fxml")));
         primaryStage.setTitle("Checkers");
         primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
         FileInputStream input = new FileInputStream("images/blackcheckers.png");
+        ImageView[]ImageViewArr  = new ImageView[12];
         Image[] blackCheckers = new Image[12];
         for (int i = 0; i < 12; i++) {
-            blackCheckers[0] = new Image(input);
-            ImageView imageView = new ImageView(blackCheckers[0]);
+            double offset = 37.5 * i;
+            blackCheckers[i] = new Image(input);
+            ImageView imageView = new ImageView(blackCheckers[i]);
+            imageView.setImage(blackCheckers[i]);
+            imageView.setLayoutX(0 + offset);
+            imageView.setLayoutY(0 + + offset);
+            imageView.setFitWidth(50);
+            imageView.setFitHeight(50);
+            ImageViewArr[i] = imageView;
         }
         FileInputStream input2 = new FileInputStream("images/redcheckers.png");
+        ImageView[]ImageViewArr2  = new ImageView[12];
         Image[] redCheckers = new Image[12];
         for (int i = 0; i < 12; i++) {
-            redCheckers[0] = new Image(input);
-            ImageView imageView = new ImageView(redCheckers[0]);
+            double offset = 37.5 * i;
+            redCheckers[i] = new Image(input2);
+            ImageView imageView = new ImageView(redCheckers[i]);
+            imageView.setLayoutX(100 + offset);
+            imageView.setLayoutY(100 + offset);
+            imageView.setFitWidth(50);
+            imageView.setFitHeight(50);
+            ImageViewArr2[i] = imageView;
+        }
+        for (int i = 0;i<ImageViewArr.length;i++)
+        {
+            root.getChildren().add(ImageViewArr[i]);
+            ImageViewArr[i].setImage(blackCheckers[i]);
+        }
+        for (int i = 0;i<ImageViewArr2.length;i++)
+        {
+            ImageViewArr2[i].setImage(imageView[i]);
+            root.getChildren().add(ImageViewArr2[i]);
         }
 
 
--- src/sample/Spaces.java
+++ src/sample/Spaces.java
@@ -5,7 +5,7 @@ import javafx.scene.shape.Rectangle;
 
 public class Spaces extends StackPane {
 
-    public Spaces(int x, int y, CheckerPiece piece, double x2, double y2, double width, double height) {
+    public Spaces(int x, int y, CheckerPiece piece, double x2, double y2, double width, double height, String img) {
         Rectangle rectangle = new Rectangle( width, height);
         rectangle.setStroke(Color.BLACK);
         if((x+y)%2 == 0) {
@@ -14,6 +14,7 @@ public class Spaces extends StackPane {
         else {
             rectangle.setFill(Color.BLACK);
         }
+        //37.5 per checker space
         setTranslateX(x2+180);
         setTranslateY(y2+80);
         getChildren().addAll( rectangle);
